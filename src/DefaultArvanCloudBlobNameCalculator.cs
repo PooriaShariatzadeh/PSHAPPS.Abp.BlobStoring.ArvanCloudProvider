4c23fc2a98651f2473854cc6ddddb0a383d90557
Pooria Shariatzadeh <pooria.shariatzadeh@gmail.com>
2024-09-26 13:45:24 +0330

    Add DefaultArvanCloudBlobNameCalculator

---
+++ src/DefaultArvanCloudBlobNameCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.BlobStoring;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace PSHAPPS.Abp.BlobStoring.ArvanCloudProvider.src
+{
+    public class DefaultArvanCloudBlobNameCalculator : IArvanCloudBlobNameCalculator, ITransientDependency
+    {
+        protected ICurrentTenant CurrentTenant { get; }
+
+        public DefaultArvanCloudBlobNameCalculator(ICurrentTenant currentTenant)
+        {
+            CurrentTenant = currentTenant;
+        }
+
+        public virtual string Calculate(BlobProviderArgs args)
+        {
+            return CurrentTenant.Id == null
+                ? $"host/{args.BlobName}"
+                : $"tenants/{CurrentTenant.Id.Value.ToString("D")}/{args.BlobName}";
+        }
+    }
+}
---
+++ src/IArvanCloudBlobNameCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.BlobStoring;
+
+namespace PSHAPPS.Abp.BlobStoring.ArvanCloudProvider.src
+{
+    public interface IArvanCloudBlobNameCalculator
+    {
+        string Calculate(BlobProviderArgs args);
+    }
+}
